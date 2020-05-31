b47689ed3d93082f93da8a3393b7224a56723d33
Felipe Haase <fahaase@uc.cl>
2020-05-30 23:06:56 -0400

    removed epochs from train function

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     percentage = int(sys.argv[1])
     epochs = int(sys.argv[2])
     mlp = neural_network.MLPRegressor(hidden_layer_sizes=(16,), solver='adam')
-    train(mlp, data_2016, data_percentage=percentage, epochs=epochs)
+    train(mlp, data_2016, data_percentage=percentage)
     print("Saving model")
     dt_now= datetime.datetime.now().isoformat()
     dump(mlp, f'mlp-{dt_now}.joblib')
